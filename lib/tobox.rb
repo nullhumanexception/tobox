7bee980938a1181415d00e4ac07ae998650202ab
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-06-17 15:37:11 +0300

    Initial commit

---
+++ lib/tobox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tobox/version"
+
+module Tobox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/tobox_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ToboxTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Tobox::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
